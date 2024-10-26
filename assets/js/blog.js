f54b7d617e56aa705f2583b6b1623b7ba85930ac
Larry Fletcher <susdolphin@Larrys-Laptop.attlocal.net>
2024-10-26 16:01:52 -0400

    final touches

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,17 +3,6 @@ const blogs=JSON.parse(localStorage.getItem("blogs")) || [];
 function loadBlogs(){
     section.textContent="";
 
-    /*
-         <article>
-                 <h3>blog title</h3>
-                 <hr>
-                 <p class="sentence">this is first blog</p>
-
-                 <p>Posted By: <span>Phil</span></p>
-            </article>
-*/
-
-//  first iteration
 section.innerHTML=`<article>
                  <h3>${blogs[0].title}</h3>
                  <hr>
@@ -29,40 +18,6 @@ section.innerHTML=`<article>
 
                  <p>Posted By: <span>${blogs[1].username}</span></p>
             </article>`
-
-//   let article = document.createElement("article");
-//   let h3 = document.createElement("h3");
-//   let hr= document.createElement("hr");
-//   let pSentence= document.createElement("p");
-//   let pPost= document.createElement("p");
-//   h3.textContent=blogs[0].title;
-//   pSentence.classList.add("sentence");
-//   pSentence.textContent=blogs[0].content;
-//   pPost.innerHTML="Posted By: <span>" + blogs[0].username+"</span>";
-//   article.appendChild(h3);
-//   article.appendChild(hr);
-//   article.appendChild(pSentence);
-//   article.appendChild( pPost);
-//   section.appendChild(article);
-
-
-// second iteration
-
-// article = document.createElement("article");
-// h3 = document.createElement("h3");
-// hr= document.createElement("hr");
-// pSentence= document.createElement("p");
-// pPost= document.createElement("p");
-// h3.textContent=blogs[1].title;
-// pSentence.classList.add("sentence");
-// pSentence.textContent=blogs[1].content;
-// pPost.innerHTML="Posted By: <span>" + blogs[1].username+"</span>";
-// article.appendChild(h3);
-// article.appendChild(hr);
-// article.appendChild(pSentence);
-// article.appendChild( pPost);
-// section.appendChild(article);
-
 
 }
 
